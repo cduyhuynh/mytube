650146252aa351d9bb52310299a0cb6e8e9c6f17
Duy Huynh <duy.huynh@saleshood.com>
2023-06-13 08:09:39 +0700

    setup frontend cable

--- app/javascript/components/Layout.jsx
+++ app/javascript/components/Layout.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { Layout, Input, Row, Col, Button } from 'antd';
+import { Layout, notification } from 'antd';
 import LoginHeader from './LoginHeader';
+import consumer from '../channels/consumer';
 
 const { Content, Footer } = Layout;
 
 export default function({content}) {
+  consumer.subscriptions.create({ channel: "VideoChannel" }, {
+    received(data) {
+      notification.info({
+        message: `New video shared by ${data.email}`,
+        description: ``
+      });
+  }});
   return (
     <Layout>
       <LoginHeader />
